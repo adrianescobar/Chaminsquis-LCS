45f17b92424f1de8136605f68a10a30248a07a28
adrian <adrian_080891@hotmail.com>
2012-01-26 18:31:20 -0400

    nuevo push

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+	/*
+		Configuracion base de datos.
+	*/
+	define("SERVER", "localhost");
+	define("USER", "root");
+	define("PASS", "102030");
+	define("DATABASE", "");
+
+
+?>
---
+++ modelo/Conection.php
@@ -0,0 +1,13 @@
+<?php 
+	require_once('../config.php');
+	class Hospital 
+	{
+		private $_id;
+		private $_name;
+		private $_address;
+		private $_province;
+		private $_telefono;
+
+		public 
+	}	
+?>
