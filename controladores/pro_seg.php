7611d56137d0ab99c545e3269b31e6ef26fe25e8
adrian <adrian_080891@hotmail.com>
2012-01-27 13:48:06 -0400

    ultimo pusheo

--- controladores/pro_seg.php
+++ controladores/pro_seg.php
@@ -3,14 +3,10 @@
 	require_once("../modelo/Insurance");
 	if(isset($_GET))
 	{
-		$insur = new Insurance();
 		$hosp = new Hospital();
 
-		$insur->find(null, array("name" => $_GET['name'] );
-
+		$hosp->find($_GET["seguro"], $_GET["provincia"]);
 		
-
-
 
 	}
 ?>
