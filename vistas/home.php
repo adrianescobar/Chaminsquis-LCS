406943a38154ff98f028fabd4a18b9f9c358b6f5
adrian <adrian_080891@hotmail.com>
2012-02-22 07:52:29 -0400

    design

--- vistas/home.php
+++ vistas/home.php
@@ -5,77 +5,42 @@
 		<title></title>
 		<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCLB5MsqeEErVnp1pHoJyxbzVhl_ZYOB5U&sensor=false">
     	</script>
-    	<script type="text/javascript" src="../js/linq.min.js"></script>
     	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     	<script type="text/javascript" src="../js/mapaGeo.js"></script>
     	<script>
     		
     		$(document).ready(function(){
     			
+    		});
 
-
-
-    			//Evento click para el boton cargar
-				$("#cargar").click(function(){
-				
-					//metodo ajax para la carga de datos
-					$.ajax({
-						url:"../controladores/hControlller.php",
-						success:function(data){
-
-							for(i=0;i<data.hospitales.length;i++)
-							{
-								$("<a>").attr("href","home.php?direccion="+data.hospitales[i].direccion).text(data.hospitales[i].nombre).addClass("rutas").appendTo("#rutas");
-								
-							}
-
-						},
-						type:'get',
-						dataType:'json'
-					});
-
-				$("<option>").attr("value","2").appendTo("#numOpciones");
-					
-				});
-				//
-			});
-    		
-
-    	</script>
+	</script>
 
     <link rel="stylesheet" href="../css/style.css">
 	</head>
-<body onload="geolocalizacion()">
-	<div>
-		
-		<div id="header">
-			<h1>Header</h1>
-		</div>
+	
+	<body onload="geolocalizacion()">
+		<div class = 'all'>
+			<div class= "header" >
+				<div class = 'inicio'>
+					<a href="home1.php"><img src="../img/icon.png"></a>
+				</div>
+			</div>
 
-		<div id="body">
+			<div id="body">
 
-			<div id="map"></div>
+				<div id="map"></div>
 
-			<div>
-				
-				<!-- <button id="cargar" onclick="alert(rutas);">Cargar2</button>
-				<select id="numOpciones">
+				<div class = 'form'>
 
-					<option value="5">Seleccione una Cantidad</option>
-					<option value="10">10</option>
-					<option value="15">15</option>
-					<option value="20">20</option>
-
-				</select> -->
-				<div id="rutas"></div>
+				</div>
 
 			</div>
 
+			<div class ="footer">
+				<div id = "title"> 
+					<h4>Copyright            Chaminsquis 2012</h4>	
+				</div>
+			</div>
 		</div>
-
-		<div id="footer">
-		</div>
-
-	</div>
-</body>
+	</body>
 </html>
